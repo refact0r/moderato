1d4aa3e6487dd6af375850792bbb696c5ac1578f
yfragment <34758569+yfragment@users.noreply.github.com>
2021-01-28 13:41:13 -0800

    Delete database.py

--- database.py
+++
@@ -1,7 +0,0 @@
-import pymongo
-
-cluster = pymongo.MongoClient("mongodb+srv://test:gSfnRVdfJgDq35fr@cluster0.8ot2g.mongodb.net/discordbot?retryWrites=true&w=majority")
-db = cluster['discordbot']
-collection = db['ping']
-
-collection.update_many({'_id': 1}, {'$set': {'time': 200}})
