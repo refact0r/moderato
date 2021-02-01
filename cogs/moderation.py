f8470b33a1869cd8bb22babd5b1b618bab99ed55
FatBanana <yifanw06@gmail.com>
2021-02-01 10:22:07 -0800

    unmute

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -59,13 +59,19 @@ class moderation(commands.Cog):
         for member in members:
             await member.remove_roles(role)
 
-    async def parse_args(self, ctx, args):
+    async def remove_role(self, members, role, msg, edit):
+        for member in members:
+            await member.remove_roles(role)
+        if msg and edit:
+            await msg.edit(content = edit)
+
+    async def parse_args(self, ctx, args, parse_time):
         members = []
         time = 0
 
         if args == "all":
             members = ctx.guild.members
-        elif len(args) == 2 and args.split(' ')[0] == "all":
+        elif len(args) == 2 and args.split(' ')[0] == "all" and parse_time:
             members = ctx.guild.members
             time = self.duration(args.split(' ')[1])
 
@@ -75,14 +81,14 @@ class moderation(commands.Cog):
         except:
             pass
             
-        if not members:
+        if not members and parse_time:
             try:
                 r = await RoleConverter().convert(ctx, args)
                 members = r.members
             except:
                 pass
 
-        if not members:
+        if not members and parse_time:
             try:
                 m = await MemberConverter().convert(ctx, ' '.join(args.split(' ')[:-1]))
                 members.append(m)
@@ -90,7 +96,7 @@ class moderation(commands.Cog):
             except:
                 pass
         
-        if not members:
+        if not members and parse_time:
             try:
                 r = await RoleConverter().convert(ctx, ' '.join(args.split(' ')[:-1]))
                 members = r.members
@@ -118,7 +124,7 @@ class moderation(commands.Cog):
                     if k:
                         if k == last:
                             dur = self.duration(last)
-                            if dur:
+                            if dur and parse_time:
                                 time = dur
                                 continue
                         try:
@@ -167,7 +173,7 @@ class moderation(commands.Cog):
                 overwrite.send_messages = False
                 await channel.set_permissions(role, overwrite=overwrite)
 
-        members, time = await self.parse_args(ctx, args)
+        members, time = await self.parse_args(ctx, args, True)
         
         if not members:
             await ctx.send("No users found.")
@@ -179,6 +185,36 @@ class moderation(commands.Cog):
         string, edit_string = self.generateMessages(members, time, "muted", "muting")
         msg = await ctx.send(string)
         await self.add_role(members, role, time, msg, edit_string)
+
+    @commands.command(aliases = ["um"], brief = "Unmutes a user.", help = "%unmute [user(s) or role(s)]")
+    async def unmute(self, ctx, *, args):
+        if not ctx.author.guild_permissions.manage_roles:
+            await ctx.send("You do not have the permissions to use this command.")
+            return
+
+        role = None
+        for r in ctx.guild.roles:
+            if r.name == "Muted" or r.name == "muted":
+                role = r
+        if not role:
+            role = await ctx.guild.create_role(name = "Muted", color = discord.Color(0x505050))
+            for channel in ctx.guild.channels:
+                overwrite = discord.PermissionOverwrite()
+                overwrite.send_messages = False
+                await channel.set_permissions(role, overwrite=overwrite)
+
+        members, time = await self.parse_args(ctx, args, False)
+        
+        if not members:
+            await ctx.send("No users found.")
+            return
+        if len(members) > 100:
+            await ctx.send("You cannot unmute more than 100 users at once.")
+            return
+
+        string, edit_string = self.generateMessages(members, time, "unmuted", "unmuting")
+        msg = await ctx.send(string)
+        await self.remove_role(members, role, msg, edit_string)
 
     @commands.command(aliases = ["f"], brief = "Prevents a user from adding reactions, sending files, sending embeds, or using external emojis.", help = "%freeze [user(s) or role(s)] (time)")
     async def freeze(self, ctx, *, args):
@@ -200,7 +236,7 @@ class moderation(commands.Cog):
                 overwrite.add_reactions = False
                 await channel.set_permissions(role, overwrite = overwrite)
 
-        members, time = await self.parse_args(ctx, args)
+        members, time = await self.parse_args(ctx, args, True)
         
         if not members:
             await ctx.send("No users found.")
