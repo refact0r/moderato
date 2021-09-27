1b83369661566b29b4ba8d374d591f36bea26d3c
FatBanana <34758569+refact0r@users.noreply.github.com>
2021-09-26 21:25:51 -0700

    fix kick color

--- utils/colors.py
+++ utils/colors.py
@@ -3,5 +3,6 @@ help_color = 0x5ce65c
 muted_color = 0x505050
 frozen_color = 0xb0b0b0
 exiled_color = 0x202020
-ban_color = 0xe68a5c
+ban_color = 0xe6735c
+kick_color = 0xe6a15c
 ping_color = 0x5cbae6
