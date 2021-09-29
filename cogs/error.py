b7c73ed2b83401dfd1b19d0ea1f8ee36a84072f8
FatBanana <yifanw06@gmail.com>
2021-09-29 15:06:28 -0700

    black formatting

--- cogs/error.py
+++ cogs/error.py
@@ -9,18 +9,17 @@ import sys
 
 
 class error(commands.Cog):
-
     def __init__(self, client):
         self.client = client
 
     @commands.Cog.listener()
     async def on_command_error(self, ctx, error):
         # if command has local error handler, return
-        if hasattr(ctx.command, 'on_error'):
+        if hasattr(ctx.command, "on_error"):
             return
 
         # get the original exception
-        error = getattr(error, 'original', error)
+        error = getattr(error, "original", error)
 
         if isinstance(error, commands.CommandNotFound):
             return
@@ -30,11 +29,16 @@ class error(commands.Cog):
             return
 
         if isinstance(error, commands.CommandOnCooldown):
-            await utils.utility.error_message(ctx, f"This command is on cooldown, please retry in `{math.ceil(error.retry_after)} seconds`.")
+            await utils.utility.error_message(
+                ctx,
+                f"This command is on cooldown, please retry in `{math.ceil(error.retry_after)} seconds`.",
+            )
             return
 
         if isinstance(error, commands.NoPrivateMessage):
-            await utils.utility.error_message(ctx, "This command cannot be used in direct messages.")
+            await utils.utility.error_message(
+                ctx, "This command cannot be used in direct messages."
+            )
             return
 
         if isinstance(error, commands.DisabledCommand):
@@ -42,8 +46,10 @@ class error(commands.Cog):
             return
 
         if isinstance(error, commands.BotMissingPermissions):
-            missing = [perm.replace('_', ' ').replace(
-                'guild', 'server').title() for perm in error.missing_perms]
+            missing = [
+                perm.replace("_", " ").replace("guild", "server").title()
+                for perm in error.missing_perms
+            ]
             if len(missing) > 1:
                 message = "I need the permissions"
             else:
@@ -53,8 +59,10 @@ class error(commands.Cog):
             return
 
         if isinstance(error, commands.MissingPermissions):
-            missing = [perm.replace('_', ' ').replace(
-                'guild', 'server').title() for perm in error.missing_perms]
+            missing = [
+                perm.replace("_", " ").replace("guild", "server").title()
+                for perm in error.missing_perms
+            ]
             if len(missing) > 1:
                 message = "You need the permissions"
             else:
@@ -64,16 +72,20 @@ class error(commands.Cog):
             return
 
         if isinstance(error, commands.CheckFailure):
-            await utils.utility.error_message(ctx, "You do not have permission to use this command.")
+            await utils.utility.error_message(
+                ctx, "You do not have permission to use this command."
+            )
             return
 
         if isinstance(error, discord.Forbidden):
-            await utils.utility.error_message(ctx, "I do not have permission to run this command.")
+            await utils.utility.error_message(
+                ctx, "I do not have permission to run this command."
+            )
 
-        print('Ignoring exception in command {}:'.format(
-            ctx.command), file=sys.stderr)
+        print("Ignoring exception in command {}:".format(ctx.command), file=sys.stderr)
         traceback.print_exception(
-            type(error), error, error.__traceback__, file=sys.stderr)
+            type(error), error, error.__traceback__, file=sys.stderr
+        )
 
 
 def setup(client):
--- utils/utility.py
+++ utils/utility.py
@@ -5,53 +5,50 @@ from utils import colors
 # parse time in seconds from string
 def parse_time(string):
     string = string.lower()
-    total_seconds = Decimal('0')
+    total_seconds = Decimal("0")
     prev_num = []
     for character in string:
         if character.isalpha():
             if prev_num:
-                num = Decimal(''.join(prev_num))
-                if character == 'd':
+                num = Decimal("".join(prev_num))
+                if character == "d":
                     total_seconds += num * 60 * 60 * 24
-                elif character == 'h':
+                elif character == "h":
                     total_seconds += num * 60 * 60
-                elif character == 'm':
+                elif character == "m":
                     total_seconds += num * 60
-                elif character == 's':
+                elif character == "s":
                     total_seconds += num
                 prev_num = []
         elif character.isnumeric():
             prev_num.append(character)
     return float(total_seconds)
 
+
 # convert time in seconds to readable string
 def time_string(seconds):
     period_seconds = [86400, 3600, 60, 1]
-    period_desc = ['days', 'hours', 'minutes', 'seconds']
-    s = ''
+    period_desc = ["days", "hours", "minutes", "seconds"]
+    s = ""
     remainder = seconds
     for i in range(len(period_seconds)):
         q, remainder = divmod(remainder, period_seconds[i])
         if q > 0:
             if s:
-                s += ' '
-            s += f'{int(q)} {period_desc[i]}'
+                s += " "
+            s += f"{int(q)} {period_desc[i]}"
             if q == 1:
                 s = s[:-1]
     return s
 
+
 # show message in embed
 async def embed_message(ctx, message, color):
-    embed = discord.Embed(
-        description = message,
-        color = color
-    )
-    return embed, await ctx.send(embed = embed)
+    embed = discord.Embed(description=message, color=color)
+    return embed, await ctx.send(embed=embed)
+
 
 # show error message in embed
 async def error_message(ctx, message):
-    embed = discord.Embed(
-        description = message,
-        color = colors.error_color
-    )
-    return embed, await ctx.send(embed = embed)
+    embed = discord.Embed(description=message, color=colors.error_color)
+    return embed, await ctx.send(embed=embed)
